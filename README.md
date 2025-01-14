e5c09d8fdffa4c6d7f17f219452680b08483b6b6
이용수 <iyongsu@iyongsuui-MacBookPro.local>
2025-01-14 11:33:26 +0900

    Fix: Ensure all source files are tracked

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # java-coding
+
+### 자바로 풀이한 코딩테스트 관련 문서입니다.
+Programmers,baekjun.
---
+++ src/changeCount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class changeCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("돈을 입력:");
+        int n = sc.nextInt();
+        int count = 0;
+        int[] coin_type = {500,100,50,10};
+        for(int i = 0; i< coin_type.length; i++){
+            int coin = coin_type[i];
+            count += n / coin; //1.500 2. 100 3.50 .4.10
+            n %= coin; //나머지로 남은값 반환
+        }
+        System.out.println(count);
+    }
+}
