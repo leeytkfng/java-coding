c076387e487e3cb4545051b6eca40cdccd009915
이용수 <dydtn61498@naver.com>
2025-02-06 17:08:35 +0900

    reverse

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+           String str = sc.next();
+           int cnt0 = 0;
+           int cnt1 = 0;
 
+           char prev = str.charAt(0); // 첫번째문자로 초기화
+
+           if(prev == '0') cnt0++;
+           else cnt1++;
+
+           for(int i = 1; i<str.length();i++){
+               if(str.charAt(i) != prev){
+                   if(str.charAt(i) == '0')cnt0++;
+                   else cnt1++;
+                   prev = str.charAt(i);
+               }
+           }
+           System.out.println(Math.min(cnt0,cnt1));
     }
 }
 
