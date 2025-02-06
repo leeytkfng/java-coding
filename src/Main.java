499a4e9d718367622d25e59b66fedc626d1ac500
이용수 <dydtn61498@naver.com>
2025-02-06 14:42:25 +0900

    CNT NUM

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int[] n = new int[3];
-        for(int i =0 ; i<3; i++){
-           n[i] = sc.nextInt();
-        }
 
-        System.out.println(Arrays.toString(n)); ///git 123
-
-
-//        int[] arr = {0,1,2,3,4,5,6,7,8,9};
-//        int[] cnt = new int[10];
-//
-//        if()
     }
-
 }
+
 
