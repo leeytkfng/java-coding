87ebdc6d371c4c6d87af90a45067405310746455
이용수 <dydtn61498@naver.com>
2025-02-17 11:41:56 +0900

    ROw

--- src/implement/ReadToRow.java
+++ src/implement/ReadToRow.java
@@ -1,2 +1,36 @@
-package implement;public class ReadToRow {
+package implement;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadToRow{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList <String> str = new ArrayList<>();
+
+        for(int i=0; i< 5; i++){
+            str.add(sc.nextLine());
+        }
+        int maxLength = 0 ;
+        for(String s : str) {
+            if(s.length() > maxLength){
+                maxLength = s.length();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int k = 0;  k < maxLength; k++){
+            for(int j = 0; j<str.size(); j++){
+                if(k <str.get(j).length()) {
+                    sb.append(str.get(j).charAt(k));
+                }
+            }
+
+        }
+        System.out.println(sb.toString());
+
+        sc.close();
+    }
 }
