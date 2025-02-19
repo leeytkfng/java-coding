7f5b67f63affe9ce6afbb68f1148c2e36434e128
이용수 <dydtn61498@naver.com>
2025-02-19 14:42:07 +0900

    dfs ex

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,59 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import com.sun.jdi.Value;
+
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static int[][] arr;
+    public static int N ; //행
+    public static int M; // 열
 
-        ArrayList <String> str = new ArrayList<>();
-
-        for(int i=0; i< 5; i++){
-            str.add(sc.nextLine());
-        }
-        int maxLength = 0 ;
-        for(String s : str) {
-            if(s.length() > maxLength){
-                maxLength = s.length();
-            }
+    public static boolean dfs(int i, int j){
+        if(i < 0 || N <= i || j <0 || M <=j) { //크기를 벗어나는경우
+            return false;
         }
 
-        StringBuilder sb = new StringBuilder();
+        if(arr[i][j] == 0){
+            arr[i][j] =1;
 
-        for(int k = 0;  k < maxLength; k++){
-            for(int j = 0; j<str.size(); j++){
-                if(k <str.get(j).length()) {
-                    sb.append(str.get(j).charAt(k));
+            dfs(i,j+1);  //이친구의 차이점은 4방면을 다조사
+            dfs(i+1,j);
+            dfs(i-1,j);
+            dfs(i,j-1); //재귀함수 호출방식으로 근처 노드들 탐색
+
+            return true;
+        }
+      return false;
+
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count=0;
+
+        System.out.println("얼음 틀 길이:" );
+        N = Integer.parseInt(br.readLine());
+        System.out.println("얼음 횡 길이:");
+        M = Integer.parseInt(br.readLine());
+
+        arr = new int[N][M];
+
+        for(int i =0; i<N; i++){
+           String[] text = br.readLine().split("");
+           for(int j= 0; j<M; j++){
+               arr[i][j] = Integer.parseInt(text[j]);
+           }
+        }
+
+        for(int i= 0; i<N;i++){
+            for(int j=0; j<M; j++){
+                if(dfs(i,j)){
+                    count++;
                 }
             }
-
         }
-        System.out.println(sb.toString());
 
-        sc.close();
+        bw.write(String.valueOf(count));
+        bw.close();
     }
 }
