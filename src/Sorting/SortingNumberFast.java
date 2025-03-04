797d09ff345270009b9ee8c2dc3cf62796059198
이용수 <dydtn61498@naver.com>
2025-03-04 16:50:04 +0900

    goog

--- src/Sorting/SortingNumberFast.java
+++ src/Sorting/SortingNumberFast.java
@@ -1,10 +1,11 @@
+package Sorting;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class SortingNumberFast {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
